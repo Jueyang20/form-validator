df809f6f293a37371423acdf39e45e9a7fee0fb8
jueyangn <jueyang.y@laogw.la>
2022-10-26 16:01:49 +0700

    FEAT: add printer to dashboadr & setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import AddUser from './components/AddUser';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FormAdd from './components/FormAdd';
 
-function App() {
+
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='addUser' element={<AddUser />} />
+          <Route path='formAdd' element={<FormAdd />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
